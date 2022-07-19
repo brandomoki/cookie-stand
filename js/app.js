10a7b4ab46a4c2f47a770febc75260634a987580
Brandon Pitts <brandomoki.bp@gmail.com>
2022-07-19 00:24:35 -0700

    added tokyo

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ let cookieHut = document.getElementById('cookie-Hut');
 
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-let seatle = {
-  city: 'Seatle',
+let seattle = {
+  city: 'Seattle',
   minCust: 23,
   maxCust: 65,
   avgCookieSale: 6.3,
@@ -44,3 +44,46 @@ ulElem.appendChild(liElem);
 
 }
 },
+seattle.render();
+
+let tokyo = {
+  city: 'Tokyo',
+  minCust: 3,
+  maxCust: 24,
+  avgCookieSale: 1.2,
+  randomCustomerPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+  },
+  cookiesBoughtPerHour: [],
+  totalCookies: 0,
+
+  calcCookies: function(){ for(let i = 0; i < hours.length; i++){
+    let cookiesJar = Math.round(this.avgCookieSale * this.randomCustomerPerHour());
+    this.cookiesBoughtPerHour.push(cookiesJar);
+    this.totalCookies += cookiesJar;
+  }
+  console.log(`total: ${this.totalCookies}`)
+},
+render: function(){
+  this.calcCookies();
+
+  let articleElem = document.createElement('article');
+  articleElem.textContent = this.city;
+  cookieHut.appendChild(articleElem);
+
+  let ulElem = document.createElement('ul');
+  cookieHut.appendChild(ulElem);
+
+  for(let i = 0; i < this.cookiesBoughtPerHour.length; i++){
+    let liElem = document.createElement('li');
+    liElem.textContent = `${hours[i]}: ${this.cookiesBoughtPerHour[i]} cookies!`;
+    ulElem.appendChild(liElem);
+}
+
+let liElem = document.createElement('li');
+liElem.textContent = `Total; ${this.totalCookies} cookies!`;
+ulElem.appendChild(liElem);
+
+}
+},
+tokyo.render();
